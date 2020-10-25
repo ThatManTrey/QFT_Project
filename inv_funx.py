463c948ea58fa94df9dfc886e8a7e75e5e88704f
ThatManTrey <trey_phil@protonmail.com>
2020-10-24 21:02:40 -0400

    inverse QFT start

---
+++ inv_funx.py
@@ -0,0 +1,21 @@
+def qft_rotations(circuit, n):
+    """Performs qft on the first n qubits in circuit (without swaps)"""
+    if n == 0:
+        return circuit
+    n -= 1
+    circuit.h(n)
+    for qubit in range(n):
+        circuit.cu1(pi/2**(n-qubit), qubit, n)
+    #note the recursion
+    qft_rotations(circuit, n)
+
+def swap_registers(circuit, n):
+    for qubit in range(n//2):
+        circuit.swap(qubit, n-qubit-1)
+    return circuit
+
+def qft(circuit, n):
+    """QFT on the first n qubits in circuit"""
+    qft_rotations(circuit, n)
+    swap_registers(circuit, n)
+    return circuit
