cc0b2f6d5d203acbe81b2cbe9684e77faaa9c847
EmilyCRobinson <46360123+EmilyCRobinson@users.noreply.github.com>
2020-10-25 02:36:19 -0400

    Create music.py

---
+++ music.py
@@ -0,0 +1,30 @@
+import wave
+import glob 
+import re
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
+files = []
+for file in glob.glob("*.wav"):
+    files.append(file)
+
+files.sort(key=natural_keys)
+
+infiles = files
+outfile = "quantum_music.wav"
+
+data= []
+for infile in infiles:
+    w = wave.open(infile, 'rb')
+    data.append( [w.getparams(), w.readframes(w.getnframes())] )
+    w.close()
+    
+output = wave.open(outfile, 'wb')
+output.setparams(data[0][0])
+for i in range(len(data)):
+    output.writeframes(data[i][1])
+output.close()
